97cd517b7dac1a230988bf22d1e7b41780993cc8
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-02-02 22:19:37 -0500

    exec Maven

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -2,23 +2,19 @@ package edu.eci.cvds.patterns.shapes;
 
 import edu.eci.cvds.patterns.shapes.concrete.*;
 
-
 public class ShapeFactory {
-    public static Shape create(RegularShapeType type){
-        switch (type){
-            case Triangle -> {
+    public static Shape create(RegularShapeType type) {
+        switch (type) {
+            case Triangle:
                 return new Triangle();
-            }
-            case Quadrilateral -> {
+            case Quadrilateral:
                 return new Quadrilateral();
-            }
-            case Pentagon -> {
+            case Pentagon:
                 return new Pentagon();
-            }
-            case Hexagon -> {
+            case Hexagon:
                 return new Hexagon();
-            }
+            default:
+                throw new IllegalArgumentException("Invalid shape type: " + type);
         }
-        return null;
     }
 }
